3df65ef9a3720fd7f6fcbc304cbcac2257e7020e
bjray <bjandraney@mac.com>
2014-02-05 16:40:44 -0600

    set up basic control layout and added placeholder tab icons

---
+++ fitlog/ActivityLogViewController.m
@@ -0,0 +1,38 @@
+//
+//  ActivityLogViewController.m
+//  fitlog
+//
+//  Created by B.J. Ray on 2/5/14.
+//  Copyright (c) 2014 109Software. All rights reserved.
+//
+
+#import "ActivityLogViewController.h"
+
+@interface ActivityLogViewController ()
+
+@end
+
+@implementation ActivityLogViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
