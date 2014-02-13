4c70914e3f237b2f30d154710dfee22e1fbc7cf6
bjray <bjandraney@mac.com>
2014-02-13 13:18:03 -0600

    add activity tableviewcell, pull data from activity manager and customize search cell

---
+++ fitlog/FLActivityItemCell.m
@@ -0,0 +1,29 @@
+//
+//  FLActivityItemCell.m
+//  fitlog
+//
+//  Created by B.J. Ray on 2/13/14.
+//  Copyright (c) 2014 109Software. All rights reserved.
+//
+
+#import "FLActivityItemCell.h"
+
+@implementation FLActivityItemCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
