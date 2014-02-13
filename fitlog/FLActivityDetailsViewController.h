965927768fe254a9952c8c71460bdde0b9c56503
bjray <bjandraney@mac.com>
2014-02-12 23:53:53 -0600

    added activityListVC and activityDetailsVC to story board and setup shell VC classes

---
+++ fitlog/FLActivityDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  FLActivityDetailsViewController.h
+//  fitlog
+//
+//  Created by B.J. Ray on 2/12/14.
+//  Copyright (c) 2014 109Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FLActivityDetailsViewController : UIViewController
+
+@end
---
+++ fitlog/FLActivityDetailsViewController.m
@@ -0,0 +1,38 @@
+//
+//  FLActivityDetailsViewController.m
+//  fitlog
+//
+//  Created by B.J. Ray on 2/12/14.
+//  Copyright (c) 2014 109Software. All rights reserved.
+//
+
+#import "FLActivityDetailsViewController.h"
+
+@interface FLActivityDetailsViewController ()
+
+@end
+
+@implementation FLActivityDetailsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
