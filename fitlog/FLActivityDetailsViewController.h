0f56f32bbfa1d36ef898784f5cbc6308f2bc002c
bjray <bjandraney@mac.com>
2014-02-15 16:32:34 -0600

    set up basic layout for activity details VC

--- fitlog/FLActivityDetailsViewController.h
+++ fitlog/FLActivityDetailsViewController.h
@@ -8,6 +8,18 @@
 
 #import <UIKit/UIKit.h>
 
-@interface FLActivityDetailsViewController : UIViewController
+@interface FLActivityDetailsViewController : UIViewController <UITextViewDelegate>
+@property (weak, nonatomic) IBOutlet UILabel *activityNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *activityDescriptionLabel;
+@property (weak, nonatomic) IBOutlet UILabel *activityCompletionDateLabel;
+@property (weak, nonatomic) IBOutlet UILabel *sessionLabel;
+@property (weak, nonatomic) IBOutlet UILabel *sessionDurationLabel;
+@property (weak, nonatomic) IBOutlet UITextView *activityCommentsTextView;
+@property (weak, nonatomic) IBOutlet UIView *commentsView;
+
+
+- (IBAction)saveHandler:(id)sender;
+- (IBAction)backgroundTap:(id)sender;
+
 
 @end
